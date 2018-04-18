5ed7aa5013604637d77e62d20ffdefb1d0e82954
hackyminer <hackyminer@gmail.com>
2018-04-19 07:11:33 +0900

    support earnings per day

--- www/app/controllers/account.js
+++ www/app/controllers/account.js
@@ -5,6 +5,8 @@ import { computed } from '@ember/object';
 export default Controller.extend({
   applicationController: inject('application'),
   stats: computed.reads('applicationController.model.stats'),
+  config: computed.reads('applicationController.config'),
+  hashrate: computed.reads('applicationController.hashrate'),
 
   roundPercent: computed('stats', 'model', {
     get() {
@@ -13,6 +15,19 @@ export default Controller.extend({
         return 0;
       }
       return percent;
+    }
+  }),
+
+  netHashrate: computed({
+    get() {
+      return this.get('hashrate');
+    }
+  }),
+
+  earnPerDay: computed('model', {
+    get() {
+      return 24 * 60 * 60 / this.get('config').BlockTime * this.get('config').BlockReward *
+        this.getWithDefault('model.hashrate') / this.get('hashrate');
     }
   })
 });
